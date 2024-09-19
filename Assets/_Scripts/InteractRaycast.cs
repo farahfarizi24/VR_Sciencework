f344a397fb41cd730cb6ce236b71f0d247a10e0d
farahfarizi24 <61811820+farahfarizi24@users.noreply.github.com>
2024-09-19 14:38:22 +1000

    InteractRaycast

--- Assets/_Scripts/InteractRaycast.cs
+++ Assets/_Scripts/InteractRaycast.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class InteractRaycast : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void FixedUpdate()
     {
-        
-    }
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        layerMask = ~layerMask;
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
     }
 }
