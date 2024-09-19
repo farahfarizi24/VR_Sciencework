f64d2b4da0fe915530751771a477d1047ac0373f
farahfarizi24 <61811820+farahfarizi24@users.noreply.github.com>
2024-09-19 13:45:21 +1000

    Additional asset

---
+++ Assets/_Scripts/DataSaving.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System;
+using UnityEngine.UI;
+using TMPro;
+using System.Runtime.CompilerServices;
+
+public class DataSaving : MonoBehaviour
+{
+    // Create a field for the save file.
+    string saveFile;
+    string theTime;
+    string theDate;
+    string userID;
+    private StreamWriter sw;
+    GameData gameData = new GameData();
+    void Awake()
+    {
+
+       
+
+
+
+    }
+    public void GetUserID(){
+
+        theDate = System.DateTime.Now.ToString("MM/dd/yyyy");
+        getTime();
+        Debug.Log(theDate + theTime);
+        // Update the path once the persistent path exists.
+        saveFile = Application.persistentDataPath + "/"+userID+"gamedata.csv";
+        Debug.Log("File is saved at:" + saveFile);
+    }
+    public void UpdateStartingData()
+    {
+        if (!File.Exists(saveFile))
+        {
+            sw = File.AppendText(saveFile);
+            gameData.Date = theDate;
+            sw.Write(gameData.Date);
+            sw.Close();
+        }
+        sw = File.AppendText(saveFile);
+        //sw.Write("\n" + user_input.text);
+        sw.Close();
+    }
+
+
+
+    public void readFile()
+    {
+        // Does the file exist?
+        if (File.Exists(saveFile))
+        {
+            // Read the entire file and save its contents.
+            string fileContents = File.ReadAllText(saveFile);
+
+            // Deserialize the JSON data 
+            //  into a pattern matching the GameData class.
+            gameData = JsonUtility.FromJson<GameData>(fileContents);
+        }
+    }
+
+    public void getTime()
+    {
+        theTime = System.DateTime.Now.ToString("hh:mm:ss");
+    }
+    public void OnStartSaveFile()
+    {
+        gameData.Date = theDate;
+        string CurrentDate = JsonUtility.ToJson(gameData.Date);
+
+        getTime();
+        gameData.startingTime = theTime;
+        string StartTime = JsonUtility.ToJson(gameData.startingTime);
+
+        writeFile(StartTime);
+        string ParticipantID = JsonUtility.ToJson(gameData.ParticipantID);
+        writeFile(ParticipantID);
+
+        
+    }
+    public void writeFile(string jsonString)
+    {
+        // Serialize the object into JSON and save string.
+       // string jsonString = JsonUtility.ToJson(gameData);
+
+        // Write JSON to file.
+        File.WriteAllText(saveFile, jsonString);
+    }
+}
+
---
+++ Assets/_Scripts/GameData.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData
+{
+
+    // Public lives
+    //public int lives;
+    public int ParticipantID =5;
+    public string Date;
+    public string startingTime;
+    public string OnObjectPickUpTime;
+    public string OnObjectPutInBasketTime;
+    public string ObjectID;
+    public string ObjectInBasketID;
+    public string checkOutTime;
+// Public highScore
+//public int highScore;
+    
+}
